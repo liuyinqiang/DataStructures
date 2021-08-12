994c6d9d14422e0691b6ca8262286008132843b1
LYQ <lyq200710@163.com>
2021-08-12 20:44:18 +0800

    abc

--- src/com/lyq/linkedlist/Josepfu.java
+++ src/com/lyq/linkedlist/Josepfu.java
@@ -4,8 +4,10 @@ public class Josepfu {
     public static void main(String[] args) {
         CircleSingleLinkedList list = new CircleSingleLinkedList();
 
-        list.addBoy(25);
+        list.addBoy(5);
         list.showBoy();
+
+        list.countBoy(1,2,5);//2->4->1->5->3
     }
 }
 
@@ -46,6 +48,57 @@ class CircleSingleLinkedList{
             }
             curBoy = curBoy.getNext(); //后移
         }
+    }
+
+
+    /**
+     *
+     * @param startNo 从第几个人开始报数
+     * @param countNum 数几下
+     * @param nums 有几个人
+     */
+    public void countBoy(int startNo, int countNum, int nums){
+        //先对数据进行校验
+        if(first == null || startNo < 1 || startNo > nums){
+            System.out.println("参数输入有误，请重新输入");
+            return;
+        }
+        //创建要给辅助指针，帮助完成小孩出圈
+        Boy helper = first;
+        //1、需求创建一个辅助指针（变量）helper,实先应该指向环形链表的最后节点
+        while (true){
+            if(helper.getNext() == first){
+                break;
+            }
+            helper = helper.getNext();
+        }
+
+
+        //2、小孩报数前，让first和helper指针移动k-1次 //k表示从第k个人开始报数
+        for (int j = 0; j < startNo - 1; j++) {
+            first = first.getNext();
+            helper = helper.getNext();
+        }
+
+        //3、当小孩报数时，让first和helper指针同时移动m-1次，然后出圈 //m表示数m次
+        //这里是个循环操作，直到圈中只有一个节点
+        while (true){
+            if (helper == first) {//说明圈中只有一个节点
+                break;
+            }
+            //让first和helper 指针同时移动countNum=1
+            for (int j = 0; j < countNum-1; j++) {
+                first = first.getNext();
+                helper = helper.getNext();
+            }
+            //这是first指向的节点，就是要除出圈的小孩节点
+            System.out.printf("小孩%d出圈\n",first.getNo());
+            //这时将first指向的小孩出圈
+            first = first.getNext();
+            helper.setNext(first);
+        }
+        System.out.printf("最后留在圈中的小孩编号%d \n",helper.getNo());
+
     }
 
 }
